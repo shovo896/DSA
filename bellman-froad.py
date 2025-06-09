1177f9a446381e7c4dd7cedeb748f147e8b50816
Ahadul Haque Shovo <shovo2522@gmail.com>
2025-06-09 09:52:59 +0600

    Add files via upload

---
+++ bellman-froad.py
@@ -0,0 +1,29 @@
+class Graph:
+    def __init__(self,vertices):
+        self.V=vertices
+        self.graph=[]
+        def add_edges(self,s,d,w):
+            self.graph.append([s,d,w])
+        def print_solution(self,dist):
+            print("vertex distance from source")
+            for i in range(self.V-1):
+                print("{0} \t\t{1}".format(i,dist[i]))
+        def bellman_ford(self,src):
+            dist=[float("Inf")]*self.V
+            dist[src]=0
+            for _ in range(self.V-1):
+                for s,d,w in self.graph:
+                    if dist[s] !=float("Inf") and dist[s] + w < dist[d]:
+                        dist[d]=dist[s]+w
+                for s,d,w in self.graph:
+                    if dist[s] !=float("Inf") and dist[s] + w < dist[d]:
+                        print("Graph contains  a negative cycle")
+                        return
+                    self.print_solution(dist)
+g=Graph(5)
+g.add_edge(0,1,5)
+g.add_edge(0,2,4)
+g.add_edge(1,3,3)
+g.add_edge(2,1,6)
+g.add_edge(3,2,2)
+g.bellman_ford(0)
