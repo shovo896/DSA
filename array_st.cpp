6b6017e7649758b6bd7621ea02efdfb0e313bed2
Shuvo <suvo10.info@gmail.com>
2026-04-27 00:24:24 +0600

    kmol

--- array_st.cpp
+++ array_st.cpp
@@ -5,8 +5,8 @@ using namespace std ;
 int main(){
        vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
        int count = count_if(arr.begin(), arr.end(), [](int x)
-                            { return x % 2 == 0; })
-                       
-                   cout<< "even cout :" << cout << endl;
+                            { return x % 2 == 0; });
+
+       cout << "Even count : " << count << endl;
        return 0;
 }
